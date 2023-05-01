672bf1e570e68c232d65f3942798763c754bf972
Nalendra Kian Rahman <nalendrakianrahman@gmail.com>
2023-05-01 22:37:16 +0700

    desain pertama untuk detailed info exercise

--- lib/UI/view/screen/detailed_exercise.dart
+++ lib/UI/view/screen/detailed_exercise.dart
@@ -1,6 +1,9 @@
-import 'package:fitguide_exercise/UI/view_model/exercise_view_model.dart';
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:youtube_data_api/models/video.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class DetailedExercise extends StatefulWidget {
   final String name;
@@ -24,19 +27,176 @@ class DetailedExercise extends StatefulWidget {
 }
 
 class _DetailedExerciseState extends State<DetailedExercise> {
+  final YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: 'u31qwQUeGuM',
+    flags: YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: Stack(
+        children: [
+          FadeInImage(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+            placeholder: AssetImage("assets/images/placeholder.png"),
+            image: AssetImage("assets/images/placeholder.png"),
+            imageErrorBuilder: (context, error, stackTrace) {
+              return Center(
+                child: Icon(Icons.broken_image),
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: buildDetails(),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).padding.top,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.black.withOpacity(0.0),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.favorite,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDetails() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.6,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, -3),
+            blurRadius: 6,
+            color: Colors.black12,
+          ),
+        ],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(widget.name),
-            Text(widget.difficulty),
-            Text(widget.equipment),
-            Text(widget.muscle),
-            Text(widget.type),
+            SizedBox(height: 16),
+            Center(
+              child: Container(
+                height: 8,
+                width: 120,
+                decoration: BoxDecoration(
+                  color: Colors.grey[600],
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              widget.name,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Exercise Type',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '${widget.type}',
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Equipment',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '${widget.equipment}',
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Muscle ',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '${widget.muscle}',
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Instruction',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8),
             Text(widget.instructions),
+            SizedBox(height: 30),
+            Text(
+              'Tutorial Video',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.grey[700],
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+            ),
+            SizedBox(height: 30),
           ],
         ),
       ),
