7b77a777db5780eaa48442c324d4e3e108829a48
Nalendra Kian Rahman <nalendrakianrahman@gmail.com>
2023-05-05 11:28:37 +0700

    membereskan fitur search page/screen

--- lib/UI/view/screen/search_screen.dart
+++ lib/UI/view/screen/search_screen.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:fitguide_exercise/UI/view/screen/detailed_exercise.dart';
+import 'package:fitguide_exercise/UI/view_model/exercise_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -10,7 +13,16 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  TextEditingController searchController = TextEditingController();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Provider.of<ExerciseViewModel>(context, listen: false).getSearch('');
+    });
+  }
+
+  final formKey = GlobalKey<FormState>();
+  // String search = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +33,31 @@ class _SearchScreenState extends State<SearchScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      Provider.of<ExerciseViewModel>(context, listen: false)
+                          .getAllExercises();
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
               Padding(
                 padding: EdgeInsets.fromLTRB(16, 24, 8, 0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(
-                      'Find an exercise',
+                      'Find an searchExercise',
                       style: TextStyle(
                         color: Colors.blue,
                         fontSize: 30,
@@ -50,6 +80,7 @@ class _SearchScreenState extends State<SearchScreen> {
               SizedBox(height: 24),
               buildSearch(),
               SizedBox(height: 24),
+              showSearchExercise(),
             ],
           ),
         ),
@@ -58,38 +89,44 @@ class _SearchScreenState extends State<SearchScreen> {
   }
 
   Widget buildSearch() {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: TextField(
-        controller: searchController,
-        decoration: InputDecoration(
-          prefixIcon: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.search,
-              color: Colors.blue,
+    final provider = Provider.of<ExerciseViewModel>(context, listen: false);
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: TextField(
+          controller: provider.searchController,
+          decoration: InputDecoration(
+            prefixIcon: IconButton(
+              onPressed: () async {
+                provider.getSearch(provider.searchController.text);
+              },
+              icon: Icon(
+                Icons.search,
+                color: Colors.blue,
+              ),
             ),
-          ),
-          hintText: 'Search...',
-          suffixIcon: IconButton(
-            onPressed: () {
-              searchController.clear();
-            },
-            icon: Icon(
-              Icons.clear,
-              color: Colors.blue,
+            hintText: 'Search...',
+            suffixIcon: IconButton(
+              onPressed: () {
+                provider.searchController.clear();
+              },
+              icon: Icon(
+                Icons.clear,
+                color: Colors.blue,
+              ),
             ),
+            border: styleBorder(),
+            enabledBorder: styleBorder(),
+            focusedBorder: styleBorder(),
+            disabledBorder: styleBorder(),
+            contentPadding: EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+            fillColor: Colors.blueGrey[100],
+            filled: true,
           ),
-          border: styleBorder(),
-          enabledBorder: styleBorder(),
-          focusedBorder: styleBorder(),
-          disabledBorder: styleBorder(),
-          contentPadding: EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          ),
-          fillColor: Colors.blueGrey[100],
-          filled: true,
         ),
       ),
     );
@@ -103,5 +140,73 @@ class _SearchScreenState extends State<SearchScreen> {
         color: Colors.black,
       ),
     );
+  }
+
+  Consumer<ExerciseViewModel> showSearchExercise() {
+    return Consumer<ExerciseViewModel>(builder: (context, provider, _) {
+      final modelView = Provider.of<ExerciseViewModel>(context, listen: false);
+
+      return GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: modelView.searchExercise.length,
+        itemBuilder: (_, index) {
+          final searchExercise = modelView.searchExercise[index];
+          return GestureDetector(
+            onTap: () async {
+              Navigator.of(context).push(PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) =>
+                    DetailedExercise(
+                  name: searchExercise.name,
+                  type: searchExercise.type,
+                  difficulty: searchExercise.difficulty,
+                  equipment: searchExercise.equipment,
+                  instructions: searchExercise.instructions,
+                  muscle: searchExercise.muscle,
+                ),
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(0.0, 1.0);
+                  const end = Offset.zero;
+                  const curve = Curves.ease;
+
+                  var tween = Tween(begin: begin, end: end)
+                      .chain(CurveTween(curve: curve));
+
+                  return SlideTransition(
+                    position: animation.drive(tween),
+                    child: child,
+                  );
+                },
+              ));
+            },
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: Card(
+                child: Column(
+                  children: [
+                    Image(
+                      image: NetworkImage(
+                          "https://cdn.pixabay.com/photo/2018/03/17/20/51/white-buildings-3235135__340.jpg"),
+                      fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 3),
+                      child: Text(
+                        searchExercise.name,
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    });
   }
 }
--- lib/UI/view_model/exercise_view_model.dart
+++ lib/UI/view_model/exercise_view_model.dart
@@ -3,12 +3,29 @@ import 'package:fitguide_exercise/core/model/exercise_model.dart';
 import 'package:flutter/material.dart';
 
 class ExerciseViewModel with ChangeNotifier {
+  bool isLoading = true;
+  String error = '';
+  String searchText = '';
+
   List<Exercise> _exercises = [];
   List<Exercise> get exercises => _exercises;
 
+  final TextEditingController _searchController = TextEditingController();
+  TextEditingController get searchController => _searchController;
+
   getAllExercises() async {
-    final c = await ExerciseApi.getExercises(0);
+    final c = await ExerciseApi.getExercises();
     _exercises = c;
+    isLoading = false;
+    notifyListeners();
+  }
+
+  List<SearchExercise> _searchExercises = [];
+  List<SearchExercise> get searchExercise => _searchExercises;
+
+  getSearch(String name) async {
+    final c = await ExerciseApi.searchExercise(name);
+    _searchExercises = c;
     notifyListeners();
   }
 }
