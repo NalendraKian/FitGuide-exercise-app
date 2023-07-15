e1abd398e114373d3853f9b795056a4c23ad9345
Nalendra Kian Rahman <nalendrakianrahman@gmail.com>
2023-07-15 12:48:20 +0700

    fix : minor fix

---
+++ lib/utils/constants/api_constant.dart
@@ -0,0 +1,13 @@
+class Api {
+  static const baseUrlExercise = 'https://api.api-ninjas.com/v1/exercises';
+  static const baseUrlCalories = 'https://api.api-ninjas.com/v1/caloriesburned';
+  static const apiKey = 'YOUR API-NINJAS API KEY HERE';
+}
+
+class YoutubeApi {
+  static const apiKey = 'YOUR YOUTUBE API KEY HERE';
+}
+
+class LocalApi {
+  // static
+}
